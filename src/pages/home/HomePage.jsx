e3f792b84e81f875508555b0719a72edf4952566
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2024-10-29 14:45:47 +0600

    discover everything component added

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import DiscoverEverything from '../../components/DiscoverEverything/DiscoverEverything';
 
 const homepage = () => {
    return (
       <div>
          <h2> this is homepage</h2>
+         <DiscoverEverything></DiscoverEverything>
       </div>
    );
 };
